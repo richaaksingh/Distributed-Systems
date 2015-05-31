4b8dd9b28bc3b773f3480ff43ba3f164f8022391
Richa Singh <Richa@Richa>
2015-05-31 00:56:21 -0400

    initial commit

--- Git_pub_sub/src/PsInt.java
+++
@@ -1,7 +0,0 @@
-
-public interface PsInt extends java.rmi.Remote {
-	public void acknowledge( int members, String msg ) throws java.rmi.RemoteException;
-	public boolean msg( String topic, String msg ) throws java.rmi.RemoteException;
-	public String getAddress()throws java.rmi.RemoteException;
-	public void setAddress(String ip)throws java.rmi.RemoteException;
-}
